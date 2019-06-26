8d3aa1a789282e637ee42065d9a51e1f16f7886c
anweicloud <d7210c@163.com>
2019-06-27 02:40:46 +0800

    Initial commit

---
+++ nacos-discovery-provider/src/main/java/com/anwei/alibaba/nacos/discovery/provider/NacosDiscoveryProviderApplication.java
@@ -0,0 +1,38 @@
+package com.anwei.alibaba.nacos.discovery.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosDiscoveryProviderApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosDiscoveryProviderApplication.class, args);
+
+        while(true) {
+            //当动态配置刷新时，会更新到 Enviroment中，因此这里每隔一秒中从Enviroment中获取配置
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            String userAge = applicationContext.getEnvironment().getProperty("user.age");
+            System.err.println("user name :" + userName + "; age: " + userAge);
+            TimeUnit.SECONDS.sleep(3);
+        }
+
+//        SpringApplication.run(NacosDiscoveryProviderApplication.class, args);
+    }
+
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{string}")
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
