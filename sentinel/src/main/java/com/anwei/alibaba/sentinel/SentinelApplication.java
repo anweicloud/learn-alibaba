ccfc904a404967ce7eb2fec2dad82a634fa39441
anweicloud <d7210c@163.com>
2019-07-02 01:33:45 +0800

    1

--- sentinel/src/main/java/com/anwei/alibaba/sentinel/SentinelApplication.java
+++ sentinel/src/main/java/com/anwei/alibaba/sentinel/SentinelApplication.java
@@ -4,11 +4,14 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SentinelApplication {
 
     public static void main(String[] args) {
